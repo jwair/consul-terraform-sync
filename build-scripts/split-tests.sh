4d5e201edea56b08ca4bfd43f1eacf1d863d1002
AJ Jwair <aj.jwair@hashicorp.com>
2022-05-13 12:15:22 -0500

    Removed shuffling (bug)

--- build-scripts/split-tests.sh
+++ build-scripts/split-tests.sh
@@ -8,7 +8,7 @@ function list_tests() {
     local build_tags=$1
     local pkg=$2
 
-    go test -tags="${build_tags}" -list . "${pkg}" | grep "^Test" | sort -R
+    go test -tags="${build_tags}" -list . "${pkg}" | grep "^Test" | sort
 }
 
 function split_list() {
