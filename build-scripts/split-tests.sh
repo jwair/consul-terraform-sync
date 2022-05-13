36770f43f9c9500c132167854a563a15f9d10161
AJ Jwair <aj.jwair@hashicorp.com>
2022-05-12 19:48:57 -0500

    added logging, fixed test results upload

--- build-scripts/split-tests.sh
+++ build-scripts/split-tests.sh
@@ -20,6 +20,8 @@ function split_list() {
 
     mkdir -p "${chunks_dir}"
     split -d -a 1 -l "${lines_per_file}" "${input_file}" "${output_dir}/chunk."
+    echo ">> Chunks files line counts:"
+    wc -l "${output_dir}/*"
 }
 
 function join_lists() {
@@ -42,11 +44,12 @@ chunks_dir=$4
 all_tests_file=$(mktemp /tmp/split.XXXXX)
 
 if [ "${chunks_count}" -gt 10 ]; then
-  echo "ERROR: Max chunks_count (10) exceeded"
+  >&2 echo ">> ERROR: Max chunks_count (10) exceeded"
   exit 1
 fi
 
 list_tests "${build_tags}" "${package}" > "${all_tests_file}"
+echo ">> Tests detected: $(wc -l "${all_tests_file}")"
 split_list "${chunks_count}" "${all_tests_file}" "${chunks_dir}"
 join_lists "${chunks_dir}"
 
